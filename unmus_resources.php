661f4dc1a0330d48db809ef679694e0aeb3ac65c
circuscode <marco.hitschler@spelzenhof.com>
2018-08-27 22:49:50 +0200

    Download Monitor Hooks removed

--- unmus_resources.php
+++ unmus_resources.php
@@ -9,11 +9,17 @@ Remove Podlove CSS @ Non Podcast Content
 */
 
 function unmus_remove_podlove_css(){
+
     if(! ( is_post_type_archive('podcast') OR ('podcast' == get_post_type() ) ) )
     {
-    wp_dequeue_style( 'podlove-frontend-css' );
-    wp_dequeue_style( 'podlove-admin-font' );
+        wp_dequeue_style( 'podlove-frontend-css' );
     }
+
+    if (! (is_admin() OR is_user_logged_in() ) ) 
+    {
+        wp_dequeue_style( 'podlove-admin-font' );
+    }
+
 }
 add_action('wp_enqueue_scripts', 'unmus_remove_podlove_css');
 
@@ -29,14 +35,5 @@ function unmus_remove_podlove_js(){
     }
 }
 add_action('wp_enqueue_scripts', 'unmus_remove_podlove_js',99);
-
-/*
-Remove Download Monitor CSS @ 
-*/
-
-function unmus_remove_download_monitor_css(){
-    wp_dequeue_style( 'dlm-frontend' );
-}
-add_action('wp_enqueue_scripts', 'unmus_remove_download_monitor_css',20);
 
 ?>
