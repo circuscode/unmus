644b350d7de113086dcdbbb9a7f1007c418cb8cd
marco <marco.hitschler@spelzenhof.com>
2023-04-24 21:14:15 +0200

    Embed Privacy

--- unmus_dataprivacy.php
+++ unmus_dataprivacy.php
@@ -1,29 +1,85 @@
 <?php
 
-/*
-Data Privacy
-*/
+/**
+ * Data Privacy
+ * 
+ * @package unmus
+ */
 
-/* Hide Data Privacy Checkbox */
+// Security: Stops code execution if WordPress is not loaded
+if (!defined('ABSPATH')) { exit; }
 
-function comment_form_hide_cookies_consent( $fields ) {
+/**
+ * Removes the cookie opt-in from comment form
+ *
+ * Filter affects that cookie will not be set
+ * 
+ * @param array Default comment fields
+ * @return array Comment fields without cookie option
+ */
+
+function unmus_comment_form_remove_cookies_consent( $fields ) {
 	unset( $fields['cookies'] );
 	return $fields;
 }
-add_filter( 'comment_form_default_fields', 'comment_form_hide_cookies_consent' );
+add_filter( 'comment_form_default_fields', 'unmus_comment_form_remove_cookies_consent' );
 
-/* Do not save IP from comment author  */
+/**
+ * Removes the comment author's IP address
+ * 
+ * Filter will be applied before IP adress is set
+ * 
+ * @param string comment author's IP address
+ * @return string empty string
+ */
 
 function unmus_remove_comment_author_ip( $comment_author_ip ) {
 	return '';
-  }
-  add_filter( 'pre_comment_user_ip', 'unmmus_remove_comment_author_ip' );
+}
+add_filter( 'pre_comment_user_ip', 'unmus_remove_comment_author_ip' );
 
-/* Do not save User Agent from comment author */
+/**
+ * Removes the comment author's user agent
+ * 
+ * Filter will be applied before user agent is set
+ * 
+ * @param string comment author's user agent
+ * @return string empty string
+ */
 
 function unmus_remove_comment_agent_string( $comment_agent ) { 
 	return '';
-  }
+}
 add_filter( 'pre_comment_user_agent', 'unmus_remove_comment_agent_string' );
+
+/**
+ * Adjust the embed overlay text
+ * 
+ * @since 0.7
+ * 
+ * @see WordPress Plugin Embed Privacy
+ * @param string Embed Overlay Content
+ * @param string Embed Provider
+ * @return string Manipulated Embed Overlay Content
+ */
+
+function unmus_manipulate_embed_overlay_text( $content, $embed_provider ) {
+
+	// 1
+	$search_for="Erfahre mehr in der";
+	$replace_with="Was dann passiert, steht in der";
+	$content=str_replace($search_for, $replace_with, $content);
+
+	// 2
+	$search_for='target="_blank"';
+	$replace_with="";
+	$content=str_replace($search_for, $replace_with, $content);
+	
+	return $content;
+
+}
+if (defined('EMBED_PRIVACY_VERSION')) {
+add_filter( 'embed_privacy_content', 'unmus_manipulate_embed_overlay_text', 10, 2);
+}
 
 ?>
---
+++ unmus_install.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * Installation, Deinstallation, Deactivation
+ * 
+ * @package unmus
+ * @since 0.7
+ */
+
+// Security: Stops code execution if WordPress is not loaded
+if (!defined('ABSPATH')) { exit; }
+
+/**
+ * Installs the plugin
+ * 
+ * Function will triggered with plugin activation
+ * 
+ */
+
+function unmus_activate () {
+	add_option('unmus_environment',"na");
+	add_option('unmus_maintenance',"0");
+	add_option('unmus_raketenstaub_amountofposts',"10");
+	add_option('unmus_zirkusliebe_amountofposts',"10");
+	add_option('unmus_ello_amountofposts',"10");
+	add_option('unmus_force_feedupdate',"0");
+	add_option('unmus_wordpress_update_auto',"0");
+	add_option('unmus_plugins_update_auto',"0");
+	add_option('unmus_themes_update_auto',"0");
+}
+
+/**
+ * Deactivates the plugin
+ * 
+ * Function will triggered with plugin deactivation
+ */
+
+function unmus_deactivate () {
+	// Code here
+}
+
+/**
+ * Deinstalls the plugin
+ * 
+ * Function will triggered with plugin deletion
+ * 
+ */
+
+function unmus_delete () {
+	delete_option('unmus_environment');
+	delete_option('unmus_maintenance');
+	delete_option('unmus_raketenstaub_amountofposts');
+	delete_option('unmus_zirkusliebe_amountofposts');
+	delete_option('unmus_ello_amountofposts');
+	delete_option('unmus_force_feedupdate');
+	delete_option('unmus_wordpress_update_auto');
+	delete_option('unmus_plugins_update_auto');
+	delete_option('unmus_themes_update_auto');
+}
+
+?>
