b915df96d71b85cb39a5becd49b3726164b28973
Marco Hitschler <marco.hitschler@spelzenhof.com>
2025-05-19 19:33:33 +0200

    Federated Comment Filter

--- unmus_activitypub.php
+++ unmus_activitypub.php
@@ -11,10 +11,10 @@
 if (!defined('ABSPATH')) { exit; }
 
 /**
- * Remove Mastodon Blog Account @ Federated Comment
+ * Remove Mastodon Account @ Federated Comment
  * 
- * Initial content remains unchanged.
  * Filter will be applied before display. 
+ * Original comment remains unchanged in database.
  * 
  * @since 0.8
  * 
@@ -28,12 +28,24 @@ function unmus_federated_comment_remove_account( $comment_text, $comment = null 
 	// Check is required to avoid execution before database insert
 	if ($comment !== null) {
 
+		// Reply
+
 		$handle='<p><a href="https://www.unmus.de/@blog" rel="ugc">@blog</a> ';
 
 		$pos=strpos($comment_text, $handle);
 
 		if($pos===0) {
 			$comment_text = substr_replace($comment_text, '<p>', $pos, strlen($handle));
+		}
+
+		// Mentions
+
+		$mention='<a href="https://www.unmus.de/@blog" rel="ugc">@blog</a> ';
+
+		$pos=strpos($comment_text, $mention);
+
+		if ($pos !== false) {
+			$comment_text = substr_replace($comment_text, '', $pos, strlen($mention));
 		}
 
 		return $comment_text;
